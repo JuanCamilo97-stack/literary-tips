76e41e10b41dab1ceb923bbca1f81f0508e5f937
Juan Atehortua <118418932+JuanCamilo97-stack@users.noreply.github.com>
2024-07-19 12:46:59 -0500

    update changes

--- src/genres/genres.controller.ts
+++ src/genres/genres.controller.ts
@@ -1,38 +1,55 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus, HttpCode } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody, ApiParam } from '@nestjs/swagger';
 import { GenresService } from './genres.service';
 import { CreateGenreDto } from './dtos/create-genre.dto';
 import { UpdateGenreDto } from './dtos/update-genre.dto';
 
+@ApiTags('genres')
 @Controller('genres')
 export class GenresController {
   constructor(private readonly genresService: GenresService) {}
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Create a new genre' })
+  @ApiResponse({ status: HttpStatus.CREATED, description: 'The genre has been successfully created.' })
+  @ApiBody({ type: CreateGenreDto })
   create(@Body() createGenreDto: CreateGenreDto) {
     return this.genresService.create(createGenreDto);
   }
 
   @Get()
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Get all genres' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Successfully retrieved genres.' })
   findAll() {
     return this.genresService.findAll();
   }
 
   @Get(':id')
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Get a genre by ID' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Successfully retrieved genre.' })
+  @ApiParam({ name: 'id', required: true, description: 'Genre ID' })
   findOne(@Param('id') id: string) {
     return this.genresService.findOne(+id);
   }
 
   @Patch(':id')
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Update a genre by ID' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Successfully updated genre.' })
+  @ApiParam({ name: 'id', required: true, description: 'Genre ID' })
+  @ApiBody({ type: UpdateGenreDto })
   update(@Param('id') id: string, @Body() updateGenreDto: UpdateGenreDto) {
     return this.genresService.update(+id, updateGenreDto);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({ summary: 'Delete a genre by ID' })
+  @ApiResponse({ status: HttpStatus.NO_CONTENT, description: 'Successfully deleted genre.' })
+  @ApiParam({ name: 'id', required: true, description: 'Genre ID' })
   remove(@Param('id') id: string) {
     return this.genresService.remove(+id);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT||3000
+  app.setGlobalPrefix("api/v1")
+
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Literary Tips API')
+    .setDescription('API documentation for the Literary Tips application.')
+    .setVersion('1.0')
+    .addTag('tips')
+    .addTag('levels')
+    .addTag('genres')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  const swaggerPath = "api/swagger"
+  SwaggerModule.setup(swaggerPath, app, document);
+
+  await app.listen(port);
 }
 bootstrap();
